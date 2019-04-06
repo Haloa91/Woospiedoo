85af10091efd3e2c8c23d6b501b7dfa29707c45f
luca.brunello@gmail.com <luca.brunello@gmail.com>
2019-04-06 12:09:42 +0200

    Salvini quotes

---
+++ SalviniQuotes/FuckNiger.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SalviniQuotes
+{
+    public class FuckNiger
+    {
+        public string _tipo { get; set; }
+        public string _ciòchechiedo { get; set; }
+
+        public FuckNiger(string tipo, string ciòchechiedo)
+        {
+            _tipo = tipo;
+            _ciòchechiedo = ciòchechiedo;
+        }
+
+        public async Task<bool> WhoAreYou()
+        {
+            List<Thread> listone = new List<Thread>();
+            for (int i = 0; i < 180; i++)
+            {
+                Thread thread1 = new Thread(WhoAreYou);
+                thread1.Start();
+
+            }
+
+            return true;
+        }
+
+        private void WhoAreYou(object obj)
+        {
+            Console.WriteLine($"hey, fucking {this._tipo} you will never have { this._ciòchechiedo}");
+
+            Thread.Sleep(10000);
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                if (i % 5 == 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkCyan;
+                }
+                if (i % 6 == 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                }
+                if (i % 9 == 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                }
+            }
+        }
+    }
+}
---
+++ SalviniQuotes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
+
+namespace SalviniQuotes
+{
+    class Program
+    {
+
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+
+
+            Console.WriteLine($"Hello Matteo, I'm just a poor  {Environment.NewLine}");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Select  {Environment.NewLine}");
+            Console.WriteLine($"black man / gay / communist  {Environment.NewLine}");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            var tipo = Console.ReadLine();
+
+            Console.WriteLine($"What 'm asking is   {Environment.NewLine}");
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine($"Select  {Environment.NewLine}");
+            Console.WriteLine($"a job/ more rights / less racism  {Environment.NewLine}");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            var ciòchechiedo = Console.ReadLine();
+
+
+
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine($" Fuck you man, fuck you, you know what? { Environment.NewLine}");
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("what?");
+
+            Thread.Sleep(6000);
+
+
+            await new FuckNiger(tipo, ciòchechiedo).WhoAreYou();
+
+
+
+
+        }
+    }
+}
