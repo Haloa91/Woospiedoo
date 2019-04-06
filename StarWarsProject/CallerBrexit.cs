1718fe56935c32a444bf3d298d60e8854b6cf315
luca.brunello@gmail.com <luca.brunello@gmail.com>
2019-04-06 13:00:14 +0200

    star wars

---
+++ StarWarsProject/CallerBrexit.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace StarWarsProject
+{
+    internal class CallerBrexit
+    {
+        public static HttpClient _client { get; set; }
+        public CallerBrexit()
+        {
+            HttpClient client = new HttpClient();
+            _client = client;
+        }
+
+        internal string Call(string protagonist)
+        {
+            _client.BaseAddress= new Uri("https://swapi.co/api/people/");
+            var response=_client.GetAsync(protagonist).GetAwaiter().GetResult();
+            JObject res = JsonConvert.DeserializeObject<JObject>(response.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+           return res.SelectToken("name").Value<string>();
+        }
+    }
+}
---
+++ StarWarsProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StarWarsProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string protagonist = Console.ReadLine();
+            Console.Write(new CallerBrexit().Call(protagonist));
+            Console.Read();
+        }
+    }
+}
